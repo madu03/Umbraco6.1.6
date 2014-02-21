c85a4660c3a307bbcff487d3fc42910fe31ca380
kphani <kphani@macrosoftindia.com>
2014-02-21 17:56:40 +0530

    Initial  Umbraco 6.1.6 push

---
+++ Umbraco/Umbraco6.1.6/Umbraco6.1.6/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Umbraco6._1._6
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
